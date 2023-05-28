01e9489bd80846f906a411bef1d765445817cd65
hasnalhocine <ahassna-star@hotmail.com>
2023-05-28 07:10:19 -0700

    write_handlers.c

--- write_handlers.c
+++ write_handlers.c
@@ -29,10 +29,10 @@ int handle_write_char(char c, char buffer[],
 	if (width > 1)
 	{
 		buffer[BUFF_SIZE - 1] = '\0';
-		for (i = 0; < width - 1; i++)
+		for (i = 0; i < width - 1; i++)
 			buffer[BUFF_SIZE - i - 2] = padd;
 
-		if (flags & F _MINUS)
+		if (flags & F_MINUS)
 			return (write(1, &buffer[0], 1) +
 					write(1, &buffer[BUFF_SIZE - i - 1], width - 1));
 		else
